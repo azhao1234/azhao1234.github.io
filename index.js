89c81266d3370f8c478948378c74722ed27703cf
azhao1234 <107458934+azhao1234@users.noreply.github.com>
2023-04-22 02:56:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||贵州|天津|山西|山东|等|各地|祝|瑶瑶|美女|生日|快乐|! ! !|');
+      S.UI.simulate('|#countdown 3||天津|贵州|山西|山东|等|各地|祝|瑶瑶|美女|生日|快乐|! ! !|');
     }
 
     S.Drawing.loop(function () {
@@ -206,7 +206,7 @@ S.UI = (function () {
         default:
           S.Shape.switchShape(S.ShapeBuilder.letter(current[0] === cmd ? 'What?' : current));
       }
-    }, 4000, sequence.length);
+    }, 3000, sequence.length);
   }
 
   function checkInputWidth(e) {
